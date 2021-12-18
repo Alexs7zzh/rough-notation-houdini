ba1e1373dccc1a4a0b89cac9354089c9d79a690d
Alex Zhou <alex.zzh.s7@gmail.com>
2021-12-18 22:57:30 +0900

    Add rough box annotation

---
+++ src/properties.js
@@ -0,0 +1,20 @@
+CSS.registerProperty({
+  name: "--roughness",
+  syntax: "<integer>",
+  inherits: false,
+  initialValue: 2
+})
+
+CSS.registerProperty({
+  name: "--rough-color",
+  syntax: "<color>",
+  inherits: false,
+  initialValue: "black"
+});
+
+CSS.registerProperty({
+  name: "--type",
+  syntax: "underline | box",
+  inherits: false,
+  initialValue: "underline"
+});
--- src/worklet.js
+++ src/worklet.js
@@ -1,27 +1,29 @@
 if (typeof registerPaint !== "undefined") {
   class RoughNotation {
     constructor() {
-      this._bowing = 1;
-      this._offset = 4;
-      this._maxRandomnessOffset = 1;
       this._seed = Math.floor(Math.random() * 2 ** 31);
     }
 
     static get inputProperties() {
-      return ['--roughness', '--rough-color'];
+      return ['--roughness', '--rough-color', '--type'];
     }
 
     paint(ctx, size, styles) {
       this._ctx = ctx
+      this._ctx.lineWidth = 1.5;
 
       this._roughness = parseInt(styles.get('--roughness')) || 2;
-      this._ctx.lineWidth = 2;
       this._ctx.strokeStyle = String(styles.get('--rough-color')) || '#000';
-
-      this.drawLine(0, size.height - this._offset, size.width, size.height - this._offset);
+      if (styles.get('--type').toString().trim() === 'box') {
+        this._offset = 6;
+        this.rectangle(this._offset, this._offset, size.width - this._offset, size.height - this._offset);
+      } else {
+        this._offset = 8;
+        this.line(0, size.height - this._offset, size.width, size.height - this._offset);
+      }
     }
 
-    drawLine(x1, y1, x2, y2) {
+    line(x1, y1, x2, y2) {
       const random = () => ((2 ** 31 - 1) & (this._seed = Math.imul(48271, this._seed))) / 2 ** 31;
       const offsetOpt = x => this._roughness * roughnessGain * ((random() * x * 2) - x);
 
@@ -31,12 +33,13 @@ if (typeof registerPaint !== "undefined") {
       if (length < 200) roughnessGain = 1;
       else if (length > 500) roughnessGain = 0.4;
       else roughnessGain = (-0.0016668) * length + 1.233334;
-      let offset = this._maxRandomnessOffset;
+      const maxRandomnessOffset = 1;
+      let offset = maxRandomnessOffset;
       if ((offset * offset * 100) > lengthSq) offset = length / 10;
       const divergePoint = 0.2 + random() * 0.2;
 
-      const midDispX = offsetOpt(this._bowing * this._maxRandomnessOffset * (y2 - y1) / 200);
-      const midDispY = offsetOpt(this._bowing * this._maxRandomnessOffset * (x1 - x2) / 200);
+      const midDispX = offsetOpt(maxRandomnessOffset * (y2 - y1) / 200);
+      const midDispY = offsetOpt(maxRandomnessOffset * (x1 - x2) / 200);
 
       const randomFull = () => offsetOpt(offset);
 
@@ -53,6 +56,19 @@ if (typeof registerPaint !== "undefined") {
       )
       this._ctx.stroke();
       this._ctx.restore();
+    }
+
+    rectangle(x, y, width, height) {
+      const points = [
+        [x, y],
+        [x + width, y],
+        [x + width, y + height],
+        [x, y + height],
+      ];
+      const len = 4;
+      for (let i = 0; i < (len - 1); i++)
+        this.line(points[i][0], points[i][1], points[i + 1][0], points[i + 1][1])
+      this.line(points[len - 1][0], points[len - 1][1], points[0][0], points[0][1])
     }
   }
 
